08856ba62051cb34d04de2a74b9dec1f8b5d3975
Azlan Malik <azlanmalik409@gmail.com>
2024-03-01 23:45:25 +0500

    updated the docker-compose and config.php to hide secrets

--- php/config.php
+++ php/config.php
@@ -1,11 +1,18 @@
 <?php
-  $hostname = "db";
-  $username = "php_docker";
-  $password = "password";
-  $dbname = "php_docker";
+  require_once __DIR__ . '/../vendor/autoload.php';
+
+  // Looing for .env at the root directory
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+  $dotenv->load();
+
+  $hostname = $_ENV['MYSQL_HOST'];
+  $username = $_ENV['MYSQL_USER'];
+  $password = $_ENV['MYSQL_PASSWORD'];
+  $dbname = $_ENV['MYSQL_DATABASE'];
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
     echo "Database connection error".mysqli_connect_error();
   }
+
 ?>
